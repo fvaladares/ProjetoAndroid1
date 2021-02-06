89024e1ec6fbfb3e4572fbd9241b1b60b344c6d6
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-06 16:09:37 -0300

    Config the SecondActivity.kt
    Extract text to strings.xml

--- app/src/main/java/dev/estudos/projetoandroid/SecondActivity.kt
+++ app/src/main/java/dev/estudos/projetoandroid/SecondActivity.kt
@@ -2,17 +2,23 @@ package dev.estudos.projetoandroid
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val btnFinalizar = findViewById<Button>(R.id.activity_second_btn)
+        title = "Second Activity"
 
-        btnFinalizar.setOnClickListener {
+        val btnFinish = findViewById<Button>(R.id.activity_second_btn)
+
+        btnFinish.setOnClickListener {
             Toast.makeText(
                 this,
                 "Essa activity será finalizada. Obrigado!!",
@@ -20,6 +26,26 @@ class SecondActivity : AppCompatActivity() {
             )
                 .show()
             this.finish()
+        }
+
+        val btnChangeText = findViewById<Button>(R.id.activity_second_btn_change_text)
+
+        btnChangeText.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Changing the text!!",
+                Toast.LENGTH_LONG
+            )
+                .show()
+
+            val tv = findViewById<TextView>(R.id.activity_second_tv_to_change)
+
+//            tv.text = """Currently date and time ${LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss"))}"""
+            tv.text = getString(
+                R.string.second_activity_new_text,
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss"))
+            )
+            tv.setTextColor(ContextCompat.getColor(this, R.color.black))
         }
     }
 }
